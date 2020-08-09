c0854e8169569d1e002eea2a568d09922654316f
Valentine Shveda <67104384+luminsky@users.noreply.github.com>
2020-08-10 01:21:34 +0300

    a

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+};
+
+// import 'zone.js/dist/zone-error';
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
